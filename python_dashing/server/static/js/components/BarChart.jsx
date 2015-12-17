75d7475c2d7ffc26f953f3629cf2d397b47b8fce
Simon Knox <simon.knox@kogan.com.au>
2015-12-18 07:29:09 +1100

    add react-d3 barchart, some placeholder fill styles for chart

--- python_dashing/server/static/js/components/BarChart.jsx
+++ python_dashing/server/static/js/components/BarChart.jsx
@@ -1,21 +1,58 @@
 import React, {Component, PropTypes} from 'react';
+import {findDOMNode} from 'react-dom';
 import styles from './Number.css';
 import WidgetBox from './WidgetBox.jsx';
-import {Bar} from 'react-chartjs';
+import {BarChart as Bar} from 'react-d3';
 
-const BarChart = ({data, title='Bar Chart'}) =>
-  <WidgetBox color="#3498db" className={styles.container}>
-    <h1 className={styles.heading}>{title}</h1>
-    <Bar data={data} width="600" height="250"/>
-  </WidgetBox>
+export default class BarChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+  }
+
+  componentDidMount() {
+    const container = findDOMNode(this);
+    this.setState({
+      containerWidth: container.clientWidth,
+      containerHeight: container.clientHeight,
+    });
+  }
+
+  render() {
+    let {data, title} = this.props;
+data = [
+  {
+    name: "series1",
+    values: [ { x: 0, y: 20 }, { x: 24, y: 10 } ],
+    strokeWidth: 3,
+    strokeDashArray: "5,5",
+  },
+  {
+    name: "series2",
+    values: [ { x: 70, y: 82 },{ x: 76, y: 82 } ]
+  }
+];
+    return (
+      <WidgetBox color="#3498db" className={styles.container}>
+        <h1 className={styles.heading}>{title}</h1>
+        <Bar className={styles.chart} data={data} width={this.state.containerWidth} height={this.state.containerHeight} />
+      </WidgetBox>
+    );
+  }
+};
 
 
 BarChart.propTypes = {
-  data: PropTypes.shape({
-    labels: PropTypes.array,
-    datasets: PropTypes.array
+  data: PropTypes.arrayOf({
+    name: PropTypes.string,
+    values: PropTypes.arrayOf({
+      x: PropTypes.number,
+      y: PropTypes.number,
+    }),
   }),
   title: PropTypes.string,
 };
 
-export default BarChart;
