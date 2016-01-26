78001f25013770d312136066daf69e5c13d01a9c
Michael Cooper <mic159@gmail.com>
2016-01-27 00:43:03 +1100

    Make it take up the full size

--- dashmat/widgets/src/Graph.jsx
+++ dashmat/widgets/src/Graph.jsx
@@ -1,70 +1,48 @@
 import React, {Component, PropTypes} from 'react';
-import styles from './Number.css';
+import styles from './Graph.css';
 import {WidgetBox} from 'Dashmat';
-//import Rickshaw from 'rickshaw';
 import ChartistGraph from 'react-chartist';
 import Chartist from 'chartist';
 
+// Put the chartist CSS into the page. Magic!
 require('style-loader!raw!chartist/dist/chartist.min.css');
 
 
 export class Graph extends Component {
   constructor(props) {
     super(props);
-    //this.state = {graph: null, width: 0, height: 0};
   }
 
-  //componentDidMount() {
-  //  const colors = ['steelblue', 'lightblue'];
-  //  const series = this.props.data.map((series, idx) => {
-  //    if (!series.color) {
-  //      series.color = colors[idx];
-  //    }
-  //    return series;
-  //  });
-  //  const graph = new Rickshaw.Graph({
-  //    element: this.refs.chart,
-  //    renderer: 'area',
-  //    stroke: true,
-  //    series: series,
-  //  });
-  //  graph.render();
-  //  this.setState({graph: graph});
-  //}
-
   render() {
-    const {options} = this.props;
-    const data = {
-      //series: [[
-      //  {x: 1, y: 100},
-      //  {x: 2, y: 50},
-      //  {x: 3, y: 25},
-      //  {x: 5, y: 12.5},
-      //  {x: 8, y: 6.25}
-      //]],
-      high: 100,
-      low: 0,
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-      series: [
-        [12, 9, 7, 8, 5],
-        [2, 1, 3.5, 7, 3],
-        [1, 3, 4, 5, 6],
-      ]
+    const {options, data} = this.props;
+    const chartData = {
+      series: data,
     };
 
     const chartConfig = {
       axisX: {
         type: Chartist.AutoScaleAxis,
-        onlyInteger: true,
-        high: 100,
-        low: 0,
+        showGrid: false,
+        offset: 0,
       },
-      height: 300,
+      axisY: {
+        showGrid: true,
+        offset: 0,
+      },
+      height: 340,
+      showPoint: false,
+      showArea: true,
+      chartPadding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
     };
 
     return (
-      <WidgetBox className={styles.container} color={options.backgroundColor}>
-        <ChartistGraph data={data} options={chartConfig} type="Line" />
+      <WidgetBox className={styles.container} color={options.backgroundColor || '#2c3e50'}>
+        <ChartistGraph data={chartData} options={chartConfig} type="Line" />
       </WidgetBox>
     );
   }
@@ -80,12 +58,5 @@ Graph.propTypes = {
   options: PropTypes.shape({
     title: PropTypes.string,
     backgroundColor: PropTypes.string,
-    colors: PropTypes.arrayOf(PropTypes.string),
   }),
-};
-
-Graph.defaultProps = {
-  options: {
-    colors: ['steelblue', 'lightblue']
-  },
 };
