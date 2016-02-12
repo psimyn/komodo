ebcb9154c51364c9975f08dc14941b353427dbed
Simon Knox <simon.knox@kogan.com.au>
2016-02-12 16:21:02 +1100

    add threshold to gauge

--- dashmat/widgets/src/Gauge.jsx
+++ dashmat/widgets/src/Gauge.jsx
@@ -1,8 +1,8 @@
 import React, {Component, PropTypes} from 'react';
-import styles from './Gauge.css';
 import {WidgetBox} from 'Dashmat';
 import ChartistGraph from 'react-chartist';
 import Chartist from 'chartist';
+import styles from './Gauge.css';
 
 export class Gauge extends Component {
   render() {
@@ -30,8 +30,10 @@ export class Gauge extends Component {
       series: [val, max - val]
     }
 
+    const backgroundColor = value < options.threshold ? '#2ecc71' : '#e74c3c'
+
     return (
-      <WidgetBox className={styles.container} color={options.backgroundColor}>
+      <WidgetBox className={styles.container} color={backgroundColor}>
         <h1 className={styles.heading}>{options.title}</h1>
         <ChartistGraph className="ct-octave" data={dataset} options={chartConfig} type="Pie" />
         <span className={styles.value}>{options.prefix}{value}{options.suffix}</span>
@@ -55,5 +57,6 @@ Gauge.defaultProps = {
   data: 0,
   options: {
     max: 100,
+    threshold: 50
   },
 };
